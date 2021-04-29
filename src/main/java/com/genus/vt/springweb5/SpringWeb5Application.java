5fba8dcd7daef04396fa29534a7af3dc1435fa08
Vent-Tardar <varvara270802@bk.ru>
2021-04-29 10:03:29 +0300

    Start

---
+++ src/main/java/com/genus/vt/springweb5/SpringWeb5Application.java
@@ -0,0 +1,13 @@
+package com.genus.vt.springweb5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeb5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/genus/vt/springweb5/SpringWeb5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genus.vt.springweb5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
